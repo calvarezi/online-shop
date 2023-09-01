fb3d1825255a64d3488f250716d021fb67017793
Camilo <calvarezi0109@outlook.com>
2023-09-01 13:50:03 -0500

    first-commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,36 @@
-$(document).ready(function(){
-    function initializeSlick() {
-      $('.carousel__group').slick({
-        slidesToShow: 4, // Cambiar a 2 para mostrar dos imágenes por grupo
-        slidesToScroll: 2,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        prevArrow: false,
-        nextArrow: false,
-        speed: 1000, // Ajustar la velocidad de transición
-      });
-    }
-  
-    initializeSlick(); // Inicializar Slick al cargar la página
-  
-    $('.carousel__group').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-      if (nextSlide === 0) {
-        $('.carousel__group').slick('slickGoTo', slick.slideCount); // Cambiar al último grupo de imágenes
-      }
+$(document).ready(function () {
+  function initializeSlick() {
+    $(".carousel__group").slick({
+      slidesToShow: 2, // Cambiar a 2 para mostrar dos imágenes por grupo
+      slidesToScroll: 2,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      prevArrow: false,
+      nextArrow: false,
+      rows: 2,
+      speed: 1000, // Ajustar la velocidad de transición
     });
+  }
+
+  initializeSlick(); // Inicializar Slick al cargar la página
+
+  $(".carousel__group").on(
+    "beforeChange",
+    function (event, slick, currentSlide, nextSlide) {
+      if (nextSlide === 0) {
+        $(".carousel__group").slick("slickGoTo", slick.slideCount); // Cambiar al último grupo de imágenes
+      }
+    }
+  );
+});
+
+$(document).ready(function(){
+  $('.carousel__item').slick({
+      infinite: true,
+      slidesToShow: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
   });
-  
+});
